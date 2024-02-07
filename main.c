ad983879d10689e5ac981a2010dfa65e9a71c196
medhedimaaroufi <149690463+medhedimaaroufi@users.noreply.github.com>
2024-02-07 22:32:35 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #define vowel "aeiouy"
 #define consonant "bcdfghjklmnpqrstvwxz"
 #define path_dict "dictionnaire.txt"
+#define path_cleanDict "cleanDict.txt"
 #define path_index "index.dat"
 
 ////// DECLARATION DES STRUCTURES //////
@@ -18,6 +19,7 @@ typedef struct index_char
     char firstChar;
     long int position;
 } index_char;  //STRUCTURE POUR L'INDEXATION DE DICTIONNAIRE
+
 typedef struct player{
     int id;
     char name[20];
@@ -25,10 +27,24 @@ typedef struct player{
     int score;
 }player;  //STRUCTURE POUR LES JOUEURS
 
+//////EFFACER LES MOTS DE LONGUEUR SUPERIEURE A 10 //////
+void CleanDict(){
+    FILE *dict=fopen(path_dict, "r");
+    FILE *cleanDict=fopen(path_cleanDict, "w");
+    char buffer[17];
+    while(fgets(buffer, sizeof(buffer), dict)){
+        if (strlen(buffer)<11)
+            fputs(buffer, cleanDict);
+
+    }
+    fclose(dict);
+    fclose(cleanDict);
+}
+
+
 ////// FONCTION POUR CREER LE FICHIER INDEX //////
-void CreateIndex(){
-    printf("inCreateIndex");
-    FILE *dict=fopen(path_dict,"r");
+void CreateIndex(char *path){
+    FILE *dict=fopen(path,"r");
     FILE *index=fopen(path_index,"wb");
     struct index_char ind_char;
     char firstChar=96;
@@ -47,37 +63,27 @@ void CreateIndex(){
     fclose(index);
 }
 
+
 ////// FONCTION POUR VERIFICATION DE PREMIER MOT DE CHAQUE LETTRE //////
-void VerifyIndex(){
-    FILE *dict=fopen(path_dict, "r");
-    FILE *index=fopen(path_index, "rb");
-    struct index_char ind_char;
+void VerifyIndex(char *path ){
+    FILE *dict=fopen(path, "r");           // OUVRIR LE FICHIER DICT EN MODE LECTURE
+    FILE *index=fopen(path_index, "rb");   // OUVRIR LE FICHIER INDEX EN MODE LECTURE BINAIRE
+    struct index_char ind_char;                           // STRUCTURE POUR STOCKER LES INDEX DE DICTIONNAIRE
     char buffer[30];
+
+    // BOUCLE POUR LIRE CHAQUE ENTREE DE L'INDEX
     while(fread (&ind_char, sizeof(struct index_char), 1, index)){
         printf("char: %c\t", ind_char.firstChar);
-        fseek(dict,ind_char.position,SEEK_SET);
+        fseek(dict,ind_char.position,SEEK_SET);     // POSITIONNER LE CHARIOT DU LECTURE DE DICT A LA POSITION INDIQUEE PAR L'INDEX
         fgets(buffer, sizeof(buffer), dict);
         printf("mot: %s\n", buffer);
     }
 }
 
-////// FONCTION POUR CHERCHER UNE MOT //////
-int FindWord(char *word){
-    FILE *dict=fopen(path_dict, "r");
-    FILE *index=fopen(path_index, "rb");
-    struct index_char ind_char;
-    char buffer[30];
-    while(fread (&ind_char, sizeof(struct index_char), 1, index))
-        if (*word==ind_char.firstChar){
-            fseek(dict, ind_char.position, SEEK_SET);
-            break;
-        }
-    while (fgets(buffer, sizeof(buffer), dict) && *word==*buffer){
-        buffer[strchr(buffer, '\n')-buffer]='\0';
-        if (!strcmp(word, buffer))
-            return 1;
-    }
-    return 0;
+////// ENTRER LE NOM DU JOUEUR //////
+void EnterName(struct player *player){
+    printf("Player_%d name : ",player->id);
+    scanf("%s",player->name);
 }
 
 ////// FONCTION POUR GENERER 10 LETTRES ALEATOIREMENT AVEC LE NBRE DE VOYELLES CHOISI //////
@@ -103,9 +109,40 @@ char *Generate10Char(int nb_vowel){
     }
 }
 
+////// FONCTION DE CONTROLE DE SAISI //////
+int Input(struct player *player,char *control){
+    int i;
+    char *S,*P;
+
+    printf("Answer %s : ", player->name);
+    scanf("%s",player->word);
+
+    S= strchr(player->word,'\n');
+    if(S!=NULL) *S='\0';
+
+    S=(char *)malloc(11);
+    strcpy(S,control);
+
+    for (i=0;i< strlen(player->word);i++){
+        P=strchr(S,player->word[i]);
+        if (P==NULL) {
+            printf("Wrong input\n");
+            return Input(player,control);
+        }
+        else{
+            *P=' ';
+        }
+    }
+
+    free(S);
+
+    return 0;
+}
+
+
 ////// FONCTION POUR COMPTER LE TEMPS //////
 int timer() {
-     int i;
+    int i;
     for (i = delay; i >= 0; --i) {
         printf("\rTime remaining: %d seconds", i);
         fflush(stdout);
@@ -115,22 +152,21 @@ int timer() {
     return 0;
 }
 
-
-////// FONCTION DE CONTROLE DE SAISI //////
-int Input(struct player *player,char *control){
-    int i;
-    char *S;
-    printf("Answer %s : ", player->name);
-    scanf("%s",player->word);
-
-    S= strchr(player->word,'\n');
-    if(S!=NULL) *S='\0';
-
-    for (i=0;i< strlen(player->word);i++){
-        if (strchr(control,player->word[i])==NULL) {
-            printf("Wrong input\n");
-            return Input(player,control);
+////// FONCTION POUR CHERCHER UNE MOT //////
+int FindWord(char *word){
+    FILE *dict=fopen(path_cleanDict, "r");
+    FILE *index=fopen(path_index, "rb");
+    struct index_char ind_char;
+    char buffer[30];
+    while(fread (&ind_char, sizeof(struct index_char), 1, index))
+        if (*word==ind_char.firstChar){
+            fseek(dict, ind_char.position, SEEK_SET);
+            break;
         }
+    while (fgets(buffer, sizeof(buffer), dict) && *word==*buffer){
+        buffer[strchr(buffer, '\n')-buffer]='\0';
+        if (!strcmp(word, buffer))
+            return 1;
     }
     return 0;
 }
@@ -138,73 +174,68 @@ int Input(struct player *player,char *control){
 //////  FONCTION POUR CALCULER LE SCORE //////
 void Score(struct player *player1,struct player *player2){
     int longWord1,longWord2,FindWord1,FindWord2;
-    printf("inScore");
 
-    FindWord1= FindWord(player1->word); //VERIFIER L'EXISTANCE DE WORD1
-    FindWord2= FindWord(player2->word); //VERIFIER L'EXISTANCE DE WORD2
+    FindWord1= FindWord(player1->word);     //VERIFIER L'EXISTANCE DE WORD1
+    FindWord2= FindWord(player2->word);     //VERIFIER L'EXISTANCE DE WORD2
 
-    longWord1=strlen(player1->word);
-    longWord2=strlen(player2->word);
+    longWord1=strlen(player1->word);      //LA LONGUEUR DU 1ER MOT
+    longWord2=strlen(player2->word);      //LA LONGUEUR DU 2EME MOT
 
-    if(longWord1>longWord2)
-        if (FindWord1)
+    if(longWord1>longWord2)                 //MOT JOUEUR 2 PLUS LONGUE
+        if (FindWord1)                      //JOUEUR 1 GAGNE LA MANCHE
             player1->score+=longWord1;
-        else
+        else                                //JOUEUR 2 GAGNE LA MANCHE
             player2->score+=longWord1;
-    else if(longWord1< longWord2)
-        if (FindWord2)
+    else if(longWord1 < longWord2)          //MOTS JOUEUR 2 PLUS LONGUE
+        if (FindWord2)                      //JOUEUR 2 GAGNE LA MANCHE
             player2->score+=longWord2;
-        else
+        else                                //JOUEUR 1 GAGNE LA MANCHE
             player1->score+=longWord2;
-    else{
-        if (FindWord1 && FindWord2) {
+    else                                    //EGALITE DES MOTS
+    {
+        if (FindWord1 && FindWord2) {       //EGALITE SCORE DE LA MANCHE
             player1->score += longWord1;
             player2->score+= longWord1;
         }
-        else if (FindWord1 && !FindWord2)
+        else if (FindWord1 && !FindWord2)   // JOUEUR 1 GAGNE LA MANCHE
             player1->score+=longWord1;
-        else if (!FindWord1 && FindWord2)
+        else if (!FindWord1 && FindWord2)   // JOUEUR 2 GAGNE LA MANCHE
             player2->score+=longWord2;
     }
 }
 
-////// ENTRER LE NOM DU JOUEUR //////
-void EnterName(struct player *player){
-    printf("Player_%d name : ",player->id);
-    scanf("%s",player->name);
-}
-
-void Manche(struct player *player1,struct player *player2){
-    int nbVowel=0; //NBRE DE VOYELLES CHOISI PAR L'UTILISATEUR
-    char *control;  //LA PLUS LONGUE MOT DE DICTIONNAIRE A 15 LETTRES
+////// FONCTION POUR LANCER UNE MANCHE //////
+void Manche(struct player *player1,struct player *player2,int round){
+    int nbVowel=0;                              //NBRE DE VOYELLES CHOISI PAR L'UTILISATEUR
+    char *control;                              //LA PLUS LONGUE MOT DE DICTIONNAIRE A 15 LETTRES
     EnterName(player1);
     EnterName(player2);
 
-
-    printf("Number of vowel: "); //SAISI DU NBRE DE VOYELLES
+    printf("--%s chooses a number of vowels ", round ? player2->name : player1->name );  //SAISI DU NBRE DE VOYELLES
     scanf("%d",&nbVowel);
 
-    control=Generate10Char(nbVowel); //GENERATION DES 10 LETTRES ALEATOIREMENT
+    control=Generate10Char(nbVowel);   //GENERATION DES 10 LETTRES ALEATOIREMENT
     printf("Generated Chars : %s\n", control);
 
     timer();
 
-    Input(&player1,control); //SAISI DES 10 LETTRES DE JOUEUR 1
-    Input(&player2,control);  //SAISI DES 10 LETTRES DE JOUEUR 2
+    Input(round ? player2 : player1 ,control);             //SAISI DES 10 LETTRES DE JOUEUR 1
+    Input(round ? player1 : player2 ,control);             //SAISI DES 10 LETTRES DE JOUEUR 2
 
-    Score(&player1,&player2); //METTRE A JOUR LE SCORE
+    Score(player1,player2);                    //METTRE A JOUR LE SCORE
 
-    printf("Score Player_1 : %d\n",player1->score);
-    printf("Score Player_2 : %d\n",player2->score);
+    printf("\nName\t %s %s \n",player1->name,player2->name);
+    printf("Score\t   %d    %d \n",player1->score,player2->score);
 
 }
 
 int main(){
     struct player player1={1}; //STRUCTURE POUR LE JOUEUR 1
     struct player player2={2}; //STRUCTURE POUR LE JOUEUR 2
-
-    CreateIndex(path_dict, path_index); //CREATION DU FICHIER INDEX
-    VerifyIndex( path_dict, path_index); //VERIFICATION DU FICHIER INDEX
+    //CleanDict();
+    //CreateIndex(path_cleanDict); //CREATION DU FICHIER INDEX
+    //VerifyIndex(path_cleanDict); //VERIFICATION DU FICHIER INDEX
+    Manche(&player1,&player2,0);
 
 
     return 0;
